7128258fbcf0dbb3ea4c0d1d4d5f2dfd0be94cbb
Vladimir Mastepanov <146621116+VladimirMastepanov@users.noreply.github.com>
2024-07-07 14:10:19 +0200

    Create index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,42 @@
+import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions';
+import * as actions from '../actions/index.js';
+
+const tasks = handleActions({
+  [actions.addTask](state, { payload: { task } }) {
+    const { byId, allIds } = state;
+    return {
+      byId: { ...byId, [task.id]: task },
+      allIds: [task.id, ...allIds],
+    };
+  },
+}, { byId: {}, allIds: [] });
+
+const tasksUIState = handleActions({
+  [actions.addTask](state, { payload: { task } }) {
+    return { ...state, [task.id]: { theme: 'light' } };
+  },
+  [actions.inverseTaskTheme](state, { payload: { task } }) {
+    const currentTheme = state[task.id].theme;
+    const mapping = {
+      dark: 'light',
+      light: 'dark',
+    };
+    return { ...state, [task.id]: { theme: mapping[currentTheme] } };
+  },
+}, {});
+
+const text = handleActions({
+  [actions.addTask]() {
+    return '';
+  },
+  [actions.updateNewTaskText](state, { payload }) {
+    return payload.text;
+  },
+}, '');
+
+export default combineReducers({
+  tasks,
+  tasksUIState,
+  text,
+});
